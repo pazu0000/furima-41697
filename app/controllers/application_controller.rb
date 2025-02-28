f8cb3c5996fb74f2aee887aef791a9e6781f992a
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-02-28 22:42:52 +0900

    必要な情報を適切に入力して「ログイン」ボタンを押すと、ユーザーのログインができること。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in,
+                                      keys: [:email])
   end
 
   def basic_auth
