65b916fc86cf77f23cee3950e958cc40e96a03e2
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-06 21:03:07 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
-  def
+  def create
+  end
+
   private
 
-    def item_params
-      params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
-                                   :shipping_day_id, :sale_price)
-    end
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :sale_price).margeU(usr_id: current_user.id)
   end
 end
