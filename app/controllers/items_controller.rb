4713f039f30a4f3daf1c04a2070070467b55f068
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-06 20:56:29 +0900

    ストロングパラメーター、バリデーション、アソシエーション、マイグレーション設定、Active Storage、 MiniMagick、 ImageProcessing
    config.active_storage.variant_processor = :mini_magickの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                   :shipping_day_id, :sale_price)
+    end
+  end
 end
