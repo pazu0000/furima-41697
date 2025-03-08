d953c82f449c0e2422ae6f4e6b895ef1cd7481f0
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-08 15:12:28 +0900

    商品出品ページへ遷移（ログイン状態）
    ログアウト状態の場合は、商品出品ページへ遷移しようとすると、ログインページへ遷移すること。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  def new
+    @item = Item.new
+  end
+
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました！'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
-                                 :shipping_day_id, :sale_price).margeU(usr_id: current_user.id)
+                                 :shipping_day_id, :sale_price, :image).marge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, description, category_id, condition_id, shipping_fee_id, prefecture_id, shipping_day_id, sale_price, user_id,
+  validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :sale_price, :user_id,
             presence: true
 end
