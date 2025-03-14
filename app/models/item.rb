d678657a8080e2451864703c14e519c5f0a42f77
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-14 21:30:23 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,5 @@ class Item < ApplicationRecord
   validates :sale_price, presence: true,
                          numericality: { only_integer: true,
                                          greater_than_or_equal_to: 300,
-                                         less_than_or_equal_to: 9_999_999 } end
+                                         less_than_or_equal_to: 9_999_999 }
+end
