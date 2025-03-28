05afeccc1e068f994721e6ec6f44f90d8ff33ff3
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-28 19:36:33 +0900

    レビュー修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,21 +1,18 @@
-const price = () => {
-window.addEventListener("turbo:load", () => {
-  const priceInput = document.getElementById("item_price");
-  window.addEventListener('turbo:render', () => {
+   const price = () => {
     const priceInput = document.getElementById("item_price");
+    const addTaxInput = document.getElementById("add-tax-price")
+    const profitInput = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
-      const tax = priceInput.value;
-      const profit = priceInput.value;
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = inputValue - tax;
 
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-      const profitDom = document.getElementById("profit")
-      profitDom.innerHTML = Math.floor(inputValue - (addTaxDom.innerHTML));
+      addTaxInput.innerHTML = tax;
+      profitInput.innerHTML = profit;
 
-    })
-});
-});
-
-};
+    });
+  };
+  
+    window.addEventListener("turbo:load", price);
+    window.addEventListener('turbo:render', price);
