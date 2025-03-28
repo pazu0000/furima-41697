671b043e1beae5a7baf3a10095d3446df7fd91e8
pazu0000 <ke0519nshi@yahoo.co.jp>
2025-03-28 20:22:57 +0900

    レビュー修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,9 @@
 const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey)
+  const publicKey = gon.public_key;
+  const payjp = Payjp(publicKey);
   const elements = payjp.elements();
 
+  
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
@@ -10,24 +11,31 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-  const form = document.getElementById('charge-form')
+
+  const form = document.getElementById('charge-form');
   form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+  
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
+        alert("カード情報が正しくありません");
       } else {
         const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        const tokenInput = document.createElement("input");
+        tokenInput.setAttribute("type", "hidden");
+        tokenInput.setAttribute("name", "token");
+        tokenInput.setAttribute("value", token);
+        form.appendChild(tokenInput);
+        form.submit(); 
       }
+
       numberElement.clear();
       expiryElement.clear();
       cvcElement.clear();
-      document.getElementById("charge-form").submit();
     });
-    e.preventDefault();
   });
 };
 
 window.addEventListener("turbo:load", pay);
-window/addEventListener("turbo:render", pay);
+window.addEventListener("turbo:render", pay);
